0837f0bf2af4f036b940d570c20d763064328b62
Doi Shintaro <c0a2210630@edu.teu.ac.jp>
2023-05-09 18:31:34 +0900

    追加機能４

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,7 +171,7 @@ def main():
     bird = Bird(3, (900, 400))
     bombs = [Bomb() for _ in range(NUM_OF_BOMBS)]
     explosions = []
-    beam = None
+    beams = []
     score = 0   # 爆弾を破壊した数
     fonto = pg.font.Font(None, 80)
 
@@ -181,7 +181,7 @@ def main():
             if event.type == pg.QUIT:
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                beam = Beam(bird)
+                beams.append(Beam(bird))
         tmr += 1
         screen.blit(bg_img, [0, 0])
         
@@ -197,19 +197,32 @@ def main():
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
 
-        if beam is not None: #ビームが存在しているとき
+        # if beams is not None: #ビームが存在しているとき
+        #     for j, bomb in enumerate(bombs):
+        #         if beam._rct.colliderect(bomb._rct):
+        #             beam = None
+        #             explosions.append(Explosion(bombs[j]))
+        #             del bombs[j]
+        #             bird.change_img(6, screen)
+        #             score += 1
+        #             break
+
+        for explosion in explosions:
+            explosion.update(screen)
+
+        for i, beam in enumerate(beams):
             beam.update(screen)
-            for i, bomb in enumerate(bombs):
+            if beam._rct[0] <= 0 or beam._rct[0] >= 1600 or beam._rct[1] <= 0 or beam._rct[1] >= 900:
+                del beams[i]
+            for j, bomb in enumerate(bombs):
                 if beam._rct.colliderect(bomb._rct):
                     beam = None
-                    explosions.append(Explosion(bombs[i]))
-                    del bombs[i]
+                    explosions.append(Explosion(bombs[j]))
+                    del bombs[j]
                     bird.change_img(6, screen)
                     score += 1
                     break
 
-        for explosion in explosions:
-            explosion.update(screen)
         
         txt = fonto.render(f"{score}point", True, (255, 255, 255))
         screen.blit(txt, [1300, 800])
